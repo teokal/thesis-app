1325b307d8542e8e2e7a65107daae9dc49894465
Thodoris Kalatzis <teo.kal@hotmail.com>
2017-04-12 01:22:03 +0300

    Add 2 routes for demo /home /user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/home' => 'application#home', as: :home_page
+  get '/user' => 'user#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
