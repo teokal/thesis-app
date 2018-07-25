b140c93019396e38334ca635a154188df354907e
teokal <teo.kal@hotmail.com>
2018-07-25 23:52:23 +0300

    Move useless parameter to variable

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,6 +1,7 @@
 class NotificationController < ApplicationController
   def notifications
-    notifications = Moodle::Api.core_fetch_notifications(contextid: params[:context_id])
+    context_id = 10 # params[:context_id]
+    notifications = Moodle::Api.core_fetch_notifications(contextid: context_id)
     if notifications.blank?
       {type: :error, message: "No new notifications."}
     else
